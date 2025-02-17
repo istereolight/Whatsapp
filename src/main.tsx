98b11f1766cd3e7d7374bbb44b8c22238a3aadda
Vitaly Ishchenko <istereolight@gmail.com>
2025-02-17 15:39:36 +0300

    second

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { Provider } from 'react-redux'
-import { store } from './app/strore.ts'
+import { store } from './app/strore'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
